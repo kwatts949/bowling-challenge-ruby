225cf870cc87db6314984cce4cb8813448c199b0
Kayleigh Watts <kwatts949@gmail.com>
2022-11-13 13:21:25 +0000

    Add methods and tests

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,23 +1,42 @@
 class Bowling
   def initialize
+    @rolls = []
+    @roll_number = 1
     @score = []
-    @frame = 1
   end
 
-  def frame(first, second)
-    if first == 10
-      strike
-    elsif first + second == 10
-      spare
+  def frame(pins)
+    if pins.include?(10)
+      @rolls << pins
+      @roll_number += 1
+      if strikescore != nil
+        @score << strikescore
+      end
+    elsif pins.sum == 10
+      @rolls << pins
+      @roll_number += 1
+      @score << sparescore
     else
-      @score << first
-      @score << second
+      @rolls << pins
+      @score << pins
+      @roll_number += 1
+      @roll_number
     end
   end
 
+  def sparescore
+    return 10 
+    # plus score from next first roll of frame
+  end
+
+  def strikescore
+    if @rolls.length == 10
+      return 300
+    end
+    # plus score from first and second roll of next frame
+  end
+
   def current_score
-    @score.sum
+    @score.flatten.sum
   end
 end
-
-
